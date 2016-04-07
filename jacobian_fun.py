92895f37bd1a6ab32ff9c3dbc7c3799d612b45e2
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2016-04-07 01:58:13 -0700

    first version of jacobian estimator

--- jacobian_fun.py
+++ jacobian_fun.py
@@ -1,21 +1,53 @@
-def jacobian(x, func):
+import numpy as np
+import logging
+from time import clock
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+DELTA = np.finfo(float).eps ** 0.333
+
+def jacobian(func, x, *args, **kwargs):
     """
     estimate Jacobian
     """
-    nx = len(x)  # degrees of freedom
-    x = np.array(x)
-    LOGGER.debug(x)
-    nnx = x[0].size
-    assert all(nnx == _.size for _ in x)
+    nargs = x.shape[0]  # degrees of freedom
+    nobs = x.size / nargs  # number of observations
+    LOGGER.debug("degrees of freedom: %d", nargs)
+    LOGGER.debug("number of observations: %d", nobs )
+    f = lambda x_: func(x_, *args, **kwargs)
     j = None  # matrix of zeros
-    delta = np.eye(nx) * DELTA
-    LOGGER.debug(delta)
-    for d in delta:
-        df = np.array(func(*(x.T + d).T)) - np.array(func(*(x.T - d).T))
-        df = np.reshape(df / DELTA / 2, (-1, 1))
+    # other ideas:
+    # delta = np.eye(nargs) * DELTA
+    # delta = np.daig(np.repeat(DELTA, nargs))
+    # delta = np.daig(DELTA * np.ones(nargs))
+    # for d in delta:
+    #    pass
+    # delta = np.array([DELTA] + [0] * (nargs - 1))
+    delta = np.pad([[DELTA]], ((0, nargs - 1), (0, 0)), 'constant')
+    delta = delta.repeat(nobs, axis=1)
+    for n in xrange(nargs):
+        dx = 2.0 * DELTA * np.tile(x[n], (nargs, 1))
+        df = (f(x * (1.0 + delta)) - f(x * (1.0 - delta))) / dx
+        # derivatives df/d_n
         if j is None:
-            j = df
-            LOGGER.debug(j)
+            j = df.reshape(1, nargs, nobs)
         else:
-            j = np.append(j, df, axis=1)  # derivatives df/d_n
+            j = np.append(j, df.reshape(1, nargs, nobs), axis=0)
+        delta = np.roll(delta, 1, axis=0)
     return j
+
+if __name__ == "__main__":
+    g = lambda x: np.array([x[0] ** 2 + 2 * x[0] * x[1] + x[1] ** 2,
+                            x[1] ** 2 - x[0] ** 2])
+    h = lambda x: np.array([(2 * (x[0] + x[1]), 2 * (x[1] + x[0])),
+                            (-2 * x[0], 2 * x[1])])
+    z = np.array([range(1, 6), range(6, 11)])
+    print g(z)
+    print "==================================================================="
+    print h(z)
+    print "==================================================================="
+    c = clock()
+    print jacobian(g, z)
+    print "elapsed time = %g [s]" % (clock() - c)
+    print "==================================================================="
